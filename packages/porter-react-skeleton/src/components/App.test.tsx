29c2169dec66fcf033098575753be7129e4eca7a
Paul <p.simkevi@gmail.com>
2021-02-12 11:42:29 +0100

    temporarily remove everything test related from the skeletons

--- packages/porter-react-skeleton/src/components/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-// TODO: fix this test and add friends
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/save to reload/i);
-  expect(linkElement).toBeInTheDocument();
-});
