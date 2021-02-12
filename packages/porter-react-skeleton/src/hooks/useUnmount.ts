fadd62ee1c52392f91cb188634cea40cffb8efe1
Paul <p.simkevi@gmail.com>
2021-02-12 15:12:31 +0100

    react-skeleton: fix `useUnmount` parameter type

--- packages/porter-react-skeleton/src/hooks/useUnmount.ts
+++ packages/porter-react-skeleton/src/hooks/useUnmount.ts
@@ -1,9 +1,9 @@
-import { EffectCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 
 /**
  * @param destructor - function, which is triggered on unmount.
  */
-export default function useUnmount(destructor: EffectCallback): void {
+export default function useUnmount(destructor: () => void): void {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => destructor, []);
 }
