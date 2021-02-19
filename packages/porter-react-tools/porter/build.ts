e029967fc234fcd6b623b2aca2ec44cbb51449b2
Paul <p.simkevi@gmail.com>
2021-02-19 21:24:21 +0100

    react/vue-tools: fix webpack.config path

--- packages/porter-react-tools/porter/build.ts
+++ packages/porter-react-tools/porter/build.ts
@@ -1,1 +1,9 @@
 export * from "@mediamonks/porter-webpack-tools/porter/build";
+
+import { builder as webpackBuilder } from "@mediamonks/porter-webpack-tools/porter/build";
+
+// will override the `builder` exported from `porter-webpack-tools`
+export const builder = {
+  ...webpackBuilder,
+  config: { default: require.resolve("../webpack.config.ts") },
+};
--- packages/porter-react-tools/porter/dev.ts
+++ packages/porter-react-tools/porter/dev.ts
@@ -1,1 +1,8 @@
 export * from "@mediamonks/porter-webpack-tools/porter/dev";
+import { builder as webpackBuilder } from "@mediamonks/porter-webpack-tools/porter/dev";
+
+// will override the `builder` exported from `porter-webpack-tools`
+export const builder = {
+  ...webpackBuilder,
+  config: { default: require.resolve("../webpack.config.ts") },
+};
