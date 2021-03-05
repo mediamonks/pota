dc731cc8dcf923571aa6689bd45c65c7bcaf6756
Paul <p.simkevi@gmail.com>
2021-03-05 14:24:10 +0100

    vue-skeleton: cleanup source code

--- packages/porter-vue-skeleton/src/App.vue
+++ packages/porter-vue-skeleton/src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <style lang="scss">
-#app {
+#root {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
