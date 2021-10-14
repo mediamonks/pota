f0443fc7827fa7e3f0813119c5934faf5c914d72
Paul <p.simkevi@gmail.com>
2021-10-14 23:42:42 +0200

    fix `.env` file support

--- skeletons/webpack/build-tools/env.js
+++ skeletons/webpack/build-tools/env.js
@@ -1,5 +1,6 @@
-// import { resolve } from "path";
-// import * as paths from "./paths.js";
+import { resolve } from "path";
+import { existsSync } from "fs";
+import * as paths from "./paths.js";
 
 let DEV_SOURCE_MAP = process.env.DEV_SOURCE_MAP || "eval-source-map";
 if (DEV_SOURCE_MAP === "false") DEV_SOURCE_MAP = false;
@@ -11,13 +12,10 @@ const USE_TYPE_CHECK = process.env.TYPE_CHECK !== "false";
 const IS_DEV = process.env.NODE_ENV === "development";
 const IS_PROD = (process.env.NODE_ENV === "production") || !IS_DEV;
 
-const USE_VERSIONING = process.env.USE_VERSIONING === "true";
-const CUSTOM_VERSION = process.env.CUSTOM_VERSION;
-
 // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
-// const ENV_FILES = [`.env.${process.env.NODE_ENV}.local`, `.env.local`, `.env`].map((file) =>
-//   resolve(paths.user, file)
-// );
+const ENV_FILES = [`.env.${process.env.NODE_ENV}.local`, `.env.local`, `.env`].map((file) =>
+  resolve(paths.user, file)
+);
 
 
 // Load environment variables from .env* files. Suppress warnings using silent
@@ -25,14 +23,13 @@ const CUSTOM_VERSION = process.env.CUSTOM_VERSION;
 // that have already been set.  Variable expansion is supported in .env files.
 // https://github.com/motdotla/dotenv
 // https://github.com/motdotla/dotenv-expand
-// for (const file of ENV_FILES) {
-//   if (await existsSync(file)) {
-//     const [dotEnvExpand, dotenv] = Promise.all([import("dotenv-expand"), import("dotenv")]);
-//     // TODO: convert to ESM loading ?
-//     console.log(`TODO: load "dotenv-expand" ?`) 
-//     dotEnvExpand(dotenv.config({ path: file }));
-//   }
-// }
+for (const file of ENV_FILES) {
+  if (existsSync(file)) {
+    const [{ default: dotEnvExpand }, { default: dotenv }] = await Promise.all([import("dotenv-expand"), import("dotenv")]);
+
+    dotEnvExpand(dotenv.config({ path: file }));
+  }
+}
 // Grab NODE_ENV and PORTER_APP_* environment variables and prepare them to be
 // injected into the application via DefinePlugin in webpack configuration.
 const POTA_APP = /^POTA_APP/i;
@@ -45,18 +42,11 @@ export function getEnv() {
         env[key] = process.env[key];
         return env;
       },
-      {
-        PUBLIC_URL: process.env.PUBLIC_URL || "/",
-        // Useful for determining whether we’re running in production mode.
-        // Most importantly, it switches React into the correct mode.
-        // NODE_ENV: process.env.NODE_ENV || "development",
-        VERSION_PATH: USE_VERSIONING && IS_PROD ? `/version/${CUSTOM_VERSION || Date.now()}` : "/",
-      }
+      { PUBLIC_URL: process.env.PUBLIC_URL || "/" }
     );
   // Stringify all values so we can feed into webpack DefinePlugin
   const stringified = {
     "process.env": Object.keys(raw).reduce((env, key) => {
-      // @ts-ignore
       env[key] = JSON.stringify(raw[key]);
       return env;
     }, {}),
