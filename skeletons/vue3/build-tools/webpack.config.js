5a3a6a3b633a8a3ad6fa345267da680359776b4a
Paul <p.simkevi@gmail.com>
2021-10-28 23:38:07 +0200

    vue3-skeleton: publish 1.0.0-rc.3

--- skeletons/vue3/build-tools/webpack.config.js
+++ skeletons/vue3/build-tools/webpack.config.js
@@ -1,24 +1,29 @@
-import webpackConfig from "@pota/webpack-skeleton/build-tools/webpack.config.js";
+import createWebpackConfig from "@pota/webpack-skeleton/build-tools/webpack.config.js";
 import { VueLoaderPlugin } from "vue-loader";
 
 
-const config = {
-  ...webpackConfig,
-  module: {
-    ...webpackConfig.module,
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: "vue-loader",
-      },
-      ...webpackConfig.module.rules
-    ],
-  },
-  resolve: {
-...webpackConfig.resolve,
-    extensions: [...webpackConfig.resolve.extensions, ".vue"],
-  },
-  plugins: [...webpackConfig.plugins, new VueLoaderPlugin()],
-};
 
-export default config;
+export default function createConfig(options) {
+  const webpackConfig = createWebpackConfig(options);
+
+  return {
+    ...webpackConfig,
+    module: {
+      ...webpackConfig.module,
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+        },
+        ...webpackConfig.module.rules
+      ],
+    },
+    resolve: {
+      ...webpackConfig.resolve,
+      extensions: [...webpackConfig.resolve.extensions, ".vue"],
+    },
+    plugins: [...webpackConfig.plugins, new VueLoaderPlugin()],
+  };
+
+}
+
