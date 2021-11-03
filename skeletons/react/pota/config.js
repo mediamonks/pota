cbaa02c64084fdc1a22ca0d14c1c29036e37fd19
Paul <p.simkevi@gmail.com>
2021-11-03 21:52:30 +0100

    react-skeleton, vue-skeleton: move project dependencies to `peerDependencies`

--- skeletons/react/pota/config.js
+++ skeletons/react/pota/config.js
@@ -1,4 +1,3 @@
 export default {
   extends: "@pota/webpack-skeleton",
-  dependencies: ['react', 'react-dom', 'web-vitas', '@types/react', '@types/react-dom'],
 }
--- skeletons/vue3/pota/config.js
+++ skeletons/vue3/pota/config.js
@@ -1,4 +1,3 @@
 export default {
   extends: "@pota/webpack-skeleton",
-  dependencies: ['vue']
 }
