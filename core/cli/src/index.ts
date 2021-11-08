1cb317d5de7c91f23717e5c44e9442e85301fef0
Paul <p.simkevi@gmail.com>
2021-11-08 22:21:34 +0100

    core-cli: publish 1.0.0-rc.9 (top-level await)

--- core/cli/src/index.ts
+++ core/cli/src/index.ts
@@ -3,44 +3,41 @@ import sade from 'sade';
 import { getCommandModules } from './commands.js';
 import { getSkeletonName } from './skeleton.js';
 
-// TODO: top level await
-(async () => {
-  const mainSkeleton = await getSkeletonName();
+const mainSkeleton = await getSkeletonName();
 
-  if (!mainSkeleton) {
-    throw new Error(
-      "no skeleton is defined in the `POTA_SKELETON` environment variable or in the pota configuration in 'package.json'",
-    );
+if (!mainSkeleton) {
+  throw new Error(
+    "no skeleton is defined in the `POTA_SKELETON` environment variable or in the pota configuration in 'package.json'",
+  );
+}
+
+const main = sade('pota');
+
+for (const module of await getCommandModules(mainSkeleton)) {
+  const { action, command, options, examples, description, skeleton } = module;
+
+  if (!action || typeof action !== "function") continue;
+
+  const program = main.command(command);
+
+  const skeletonString = `[${skeleton}]`;
+
+  program.describe(
+    description
+      ? typeof description === 'string'
+        ? `${description} ${skeletonString}`
+        : [...description, skeletonString]
+      : skeletonString,
+  );
+
+  if (options) {
+    for (const o of options) program.option(o.option, o.description, o.default);
+  }
+  if (examples) {
+    for (const example of examples) program.example(example);
   }
 
-  const main = sade('pota');
+  program.action(action);
+}
 
-  for (const module of await getCommandModules(mainSkeleton)) {
-    const { action, command, options, examples, description, skeleton } = module;
-
-    if (!action || typeof action !== "function") continue;
-
-    const program = main.command(command);
-
-    const skeletonString = `[${skeleton}]`;
-
-    program.describe(
-      description
-        ? typeof description === 'string'
-          ? `${description} ${skeletonString}`
-          : [...description, skeletonString]
-        : skeletonString,
-    );
-
-    if (options) {
-      for (const o of options) program.option(o.option, o.description, o.default);
-    }
-    if (examples) {
-      for (const example of examples) program.example(example);
-    }
-
-    program.action(action);
-  }
-
-  main.parse(process.argv);
-})();
+main.parse(process.argv);
