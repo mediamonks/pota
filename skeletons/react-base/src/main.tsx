f63b7b5385e7a396c7d265e73004d953f1b553c4
Paul <p.simkevi@gmail.com>
2021-11-15 21:32:44 +0100

    [react-base|react]-skeleton: move `web-vitals` to `react-skeleton`

--- skeletons/react-base/src/main.tsx
+++ skeletons/react-base/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <StrictMode>
@@ -10,8 +9,3 @@ ReactDOM.render(
   // eslint-disable-next-line unicorn/prefer-query-selector
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
