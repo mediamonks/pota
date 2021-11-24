3449d1b843120a07f5b38b98df745c4d11b5a6e5
Paul <p.simkevi@gmail.com>
2021-11-24 12:53:06 +0200

    vue-base-skeleton: fix `dev` import from `webpack-skeleton`

--- skeletons/vue-base/.pota/commands/dev.js
+++ skeletons/vue-base/.pota/commands/dev.js
@@ -1,6 +1,6 @@
-import { options as webpackSkeletonOptions } from "@pota/webpack-skeleton/pota/commands/dev.js"
+import { options as webpackSkeletonOptions } from "@pota/webpack-skeleton/.pota/commands/dev.js"
 
-export { description, action } from "@pota/webpack-skeleton/pota/commands/dev.js"
+export { description, action } from "@pota/webpack-skeleton/.pota/commands/dev.js"
 
 import commonOptions from "../internal/commonOptions.js"
 
