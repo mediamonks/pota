8c098ef5ffff50980c19f08e0db4c9e6860821b9
Paul <p.simkevi@gmail.com>
2021-12-02 16:33:09 +0200

    webpack-skeleton: enable `bugfixes`

--- skeletons/webpack/.pota/webpack/babel.config.js
+++ skeletons/webpack/.pota/webpack/babel.config.js
@@ -4,6 +4,7 @@ export default {
       "@babel/preset-env",
       {
         useBuiltIns: "usage",
+        bugfixes: true,
         corejs: 3, // Set the corejs version we are using to avoid warnings in console
       },
     ],
