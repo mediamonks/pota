7492a3a11759cddc256d6861922ed2d64ffdcb0e
Paul <p.simkevi@gmail.com>
2021-12-02 16:32:40 +0200

    webpack-skeleton: change `useBuiltIns` to `usage`

--- skeletons/webpack/.pota/webpack/babel.config.js
+++ skeletons/webpack/.pota/webpack/babel.config.js
@@ -3,10 +3,8 @@ export default {
     [
       "@babel/preset-env",
       {
-        // Allow importing core-js in entrypoint and use browserlist to select polyfills
-        useBuiltIns: "entry",
-        // Set the corejs version we are using to avoid warnings in console
-        corejs: 3,
+        useBuiltIns: "usage",
+        corejs: 3, // Set the corejs version we are using to avoid warnings in console
       },
     ],
   ],
