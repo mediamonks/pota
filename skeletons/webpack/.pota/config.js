cf7db66e895ef26117da1d565b51646bae4e7b61
Paul <p.simkevi@gmail.com>
2021-12-02 20:31:13 +0200

    webpack-skeleton: fix `typecheck` script inclusion

--- skeletons/webpack/.pota/config.js
+++ skeletons/webpack/.pota/config.js
@@ -1,5 +1,5 @@
 export default {
-  scripts: ['check-types', 'fix', 'fix:eslint', 'fix:prettier', 'lint', 'lint:eslint'],
+  scripts: ['typecheck', 'fix', 'fix:eslint', 'fix:prettier', 'lint', 'lint:eslint'],
   rename: {
     gitignore: '.gitignore',
   },
