65ae8d553024c5a29285026ce8a9105d0bfdce68
Paul <p.simkevi@gmail.com>
2022-01-03 16:55:03 +0200

    webpack-skeleton: make sure `format` script gets included in project

--- skeletons/webpack/.pota/config.js
+++ skeletons/webpack/.pota/config.js
@@ -1,5 +1,5 @@
 export default {
-  scripts: ['typecheck', 'fix', 'fix:eslint', 'fix:prettier', 'lint', 'lint:eslint', 'rsync'],
+  scripts: ['typecheck', 'fix', 'fix:eslint', 'format', 'lint', 'lint:eslint', 'rsync'],
   rename: {
     gitignore: '.gitignore',
   },
