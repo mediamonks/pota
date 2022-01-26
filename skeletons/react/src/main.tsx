ae5e1ad97b7a2631f4c8d5041d6a150d024018af
Paul <p.simkevi@gmail.com>
2022-01-26 13:18:17 +0200

    react-skeleton: add support for service workers

--- skeletons/react/src/main.tsx
+++ skeletons/react/src/main.tsx
@@ -9,6 +9,8 @@ import GlobalStyle from './components/unlisted/GlobalStyle';
 import { GlobalStoreProvider } from './stores/global';
 import theme from './config/theme';
 
+
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
 import reportWebVitals from './reportWebVitals';
 
 /** SETUP */
@@ -41,3 +43,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
