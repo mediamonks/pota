5033030da12f2ad2848346a80c3d254056d2cd52
Paul <p.simkevi@gmail.com>
2022-01-26 21:59:55 +0200

    vue-base-skeleton: add support for service workers

--- skeletons/vue-base/src/main.ts
+++ skeletons/vue-base/src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
 
 createApp(App).mount("#root");
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
