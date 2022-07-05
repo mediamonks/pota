f28391784fb9baaee85732dfc5482dda2c020580
Arjan van Wijk <thanarie@gmail.com>
2022-07-05 12:35:21 +0200

    Add code comments to pota-cli source
    It helped me to understand the code better,
    and it should help others as well.

--- core/cli/src/createCommand.ts
+++ core/cli/src/createCommand.ts
@@ -7,6 +7,12 @@ import sade from 'sade';
 import { Command, CommandConstructor, CommandFunction, CommandModule } from './authoring.js';
 import { CommandModuleWithPath } from './loadCommands.js';
 
+/**
+ * Register the command description to the cli
+ *
+ * @param program
+ * @param command
+ */
 function applyDescription(
   program: sade.Sade,
   command: Command<Record<string, unknown>, Record<string, unknown>>,
@@ -16,6 +22,13 @@ function applyDescription(
   program.describe(command.description);
 }
 
+/**
+ * Commands have an `options` getter/property that define which options it supports,
+ * which are configured as part of pota-cli
+ *
+ * @param program
+ * @param command
+ */
 function applyOptions(
   program: sade.Sade,
   command: Command<Record<string, unknown>, Record<string, unknown>>,
@@ -28,6 +41,12 @@ function applyOptions(
   }
 }
 
+/**
+ * Register examples from te command to the cli
+ *
+ * @param program
+ * @param command
+ */
 function applyExamples(
   program: sade.Sade,
   command: Command<Record<string, unknown>, Record<string, unknown>>,
@@ -39,18 +58,39 @@ function applyExamples(
   }
 }
 
-function resolveLocalPath(path: string, cwd: string) {
+/**
+ * Resolves the path to a dependency when it would be present in the project folder, based on convention.
+ *
+ * If we have a `config.js` dependency, it will try to load `{project_folder}/pota.config.js`.
+ *
+ * @param {string} path - The path to the file you want to copy.
+ * @param {string} cwd - The current working directory.
+ * @returns The local path to the dependency using the `pota.{dependency}.(m|c)?js` convention
+ */
+function resolveLocalPath(path: string, cwd: string): string {
   return resolve(cwd, `pota.${basename(path)}`);
 }
 
+
+/**
+ * Resolves the path to a dependency based on the packagePath
+ * - If it's a "package", resolve to a package export
+ * - If it's a "project folder", resolve it to the js file
+ *
+ * This is to support running pota-cli from the project repo
+ *
+ * @param {string} path - the path to the file that we want to resolve
+ * @param {string} packagePath - the path to the package that is being resolved
+ * @returns The path to the dependency file.
+ */
 function resolvePackagePath(path: string, packagePath: string) {
-  // if the path is absolute, then its for a local module
+  // if the packagePath is absolute, then it's for a local module
   // @example
   // 'config.js' and '/home/user/pota/scripts/webpack/lib/index.js', become
   // '/home/user/pota/scripts/webpack/lib/config.js'
   if (isAbsolute(packagePath)) return resolve(dirname(packagePath), path);
 
-  // if the path is not absolute, then assume that its an installed npm package
+  // if the packagePath is not absolute, then assume that it's an installed npm package
   // @example
   // 'config.js' and '@pota/webpack-scripts', become
   // '@pota/webpack-scripts/config'
@@ -58,31 +98,51 @@ function resolvePackagePath(path: string, packagePath: string) {
   return `${packagePath}/${file}`;
 }
 
+/**
+ *
+ *
+ * @param dependencyPaths - A map of dependency names to paths.
+ * @param {string} cwd - The current working directory of the process.
+ * @param {string} packagePath - The path to the package.json file.
+ * @param {unknown} options - The options object passed to the command on execution.
+ * @returns A dictionary of the loaded dependencies
+ */
 async function loadDependencies(
   dependencyPaths: Record<string, string | ReadonlyArray<string>>,
   cwd: string,
   packagePath: string,
   options: unknown,
-) {
+): Promise<Record<string, unknown>> {
   const loadedDependencies: Record<string, unknown> = {};
 
   for (const [dependency, path] of Object.entries(dependencyPaths)) {
+    // resolve the configured paths to ones that can be imported
+    // depending on the format, multiple variations / locations of the dependency are tried
     const paths = (Array.isArray(path) ? path : [path])
       .flatMap((p) =>
+        // if the path is already resolve, use that
+        // otherwise return both the local and the package path.
+        // The cwd path is tried first, and if it doesn't exist, the package path is tried after
         isAbsolute(p) ? p : [resolveLocalPath(p, cwd), resolvePackagePath(p, packagePath)],
       )
+      // make sure the file path is in a format that can be imported
       .map((p) => (isAbsolute(p) ? pathToFileURL(p).toString() : p));
 
     let error: Error | null = null;
     for (const path of paths) {
       try {
         loadedDependencies[dependency] = (await import(path as string)).default(options);
+        // if the file exist, and doesn't throw errors, use this one and ignore the other paths for this dependency
         if (loadedDependencies[dependency]) break;
-      } catch (error) {
-        if ((error as { code: 'ERR_MODULE_NOT_FOUND' }).code !== 'ERR_MODULE_NOT_FOUND') {
+      } catch (importError: any) {
+        // TODO: if this is an error we are expecting, why do we log it?
+        if ((importError as { code: 'ERR_MODULE_NOT_FOUND' }).code !== 'ERR_MODULE_NOT_FOUND') {
           console.warn(`Error loading dependency '${path}':`);
-          console.warn(error);
-        } else error = error;
+          console.warn(importError);
+        } else {
+          // this error is only reported when none of the dependency files could be loaded
+          error = importError;
+        }
       }
     }
 
@@ -92,6 +152,18 @@ async function loadDependencies(
   return loadedDependencies;
 }
 
+
+/**
+ * Register the action with pota-cli.
+ * The action is the implementation / functionality of the command.
+ * An action can have dependencies (other files) that are loaded and provided to the action before execution.
+ *
+ *
+ * @param program - sade.Sade - The program object that we're adding the command to.
+ * @param command - The command object that we're applying to the program.
+ * @param {string} cwd - The current working directory of the command.
+ * @param {string} commandModulePath - The path to the command module.
+ */
 function applyAction(
   program: sade.Sade,
   command: Command<Record<string, unknown>, Record<string, unknown>>,
@@ -99,6 +171,10 @@ function applyAction(
   commandModulePath: string,
 ) {
   program.action(async (options) => {
+    // load external dependencies, so they can be provided to the action on execution
+    // - dependencies are configured using the `dependsOn` dictionary field in the command.
+    // - each dependency is a location to a file, or an Array of paths
+    // - when loading each dependency, it receives the command options
     const dependencies = await loadDependencies(
       command.dependsOn ?? {},
       cwd,
@@ -110,11 +186,33 @@ function applyAction(
   });
 }
 
+/**
+ * Determine if the passed Command is a class or function
+ *
+ * @param {CommandModule} value - CommandModule
+ * @returns A boolean value.
+ */
 function isNativeClass(value: CommandModule): value is CommandConstructor {
+  // If the value is a function and the function's string representation starts with the word 'class',
+  // then it's a native class.
   return typeof value === 'function' && value.toString().indexOf('class') === 0;
 }
 
-export function createCommand(main: sade.Sade, cwd: string, commandModule: CommandModuleWithPath) {
+/**
+ * Creates a "sade command" to use in pota-cli from a command specification in one of the pota scripts.
+ * Commands can be either classes or functions, and must adhere to a specific interface.
+ *
+ * Class commands can also be extended to slightly modify their parent's implementation.
+ * This is great for reusing common logic, but make slide variations for specific scripts, languages, frameworks,
+ * or in projects for project-specific requirements.
+ *
+ * @param main - sade.Sade - the main sade instance
+ * @param {string} cwd - the current working directory
+ * @param {CommandModuleWithPath} commandModule - The command module that we're creating a command for.
+ */
+export function createCommand(main: sade.Sade, cwd: string, commandModule: CommandModuleWithPath): void {
+  // functions can be used as a simplified command, missing descriptions, options, etc.
+  // they can make use of the global pota options, but otherwise they are just executed.
   if (!isNativeClass(commandModule)) {
     const commandAction: CommandFunction = commandModule;
     // use the param-case name of the function as the command name
--- core/cli/src/loadCommands.ts
+++ core/cli/src/loadCommands.ts
@@ -2,20 +2,41 @@ import { pathToFileURL } from 'url';
 import { resolve, isAbsolute, extname } from 'path';
 import { CommandModule } from './authoring.js';
 
-export function resolveModulePath(path: string, root: string) {
-  if (extname(path)) return isAbsolute(path) ? pathToFileURL(path).toString() : resolve(root, path);
+/**
+ * Resolve a user-provided path to a module, and resolve it to make it importable.
+ *
+ * @param {string} path - The path to the module.
+ * @param {string} root - The root directory of the project.
+ * @returns The resolved path to the module.
+ */
+export function resolveModulePath(path: string, root: string): string {
+  // If the path has an extension, then it's a file path, so we resolve it relative to the root directory.
+  if (extname(path)) {
+    return isAbsolute(path) ? pathToFileURL(path).toString() : resolve(root, path);
+  }
 
-  // if the path does not have an extension, then assume its a npm package
+  // If the path does not have an extension, then it's an npm package, so we just return the path
   return path;
 }
 
 export type CommandModuleWithPath = CommandModule & { path: string };
 
-export async function loadCommands(cwd: string, paths: string[]) {
+/**
+ * From a list of "pota script paths", return a dictionary with export commands.
+ * These will be registered to the pota-cli to be used.
+ * Commands with the same name will overwrite previous ones to allow the project to replace or extend pota scripts for
+ * project-specific requirements.
+ *
+ * @param {string} cwd - The current working directory of the process, usually the project root.
+ * @param {string[]} paths - An array of paths to load commands from.
+ * @returns A promise that resolves to a record of command modules with their paths.
+ */
+export async function loadCommands(cwd: string, paths: string[]): Promise<Record<string, CommandModuleWithPath>> {
   const commands: Record<string, CommandModuleWithPath> = {};
 
   for (const path of paths) {
     const resolvedPath = resolveModulePath(path, cwd);
+    // this contains all the exported classes or functions that are treated as pota commands
     const modules = await import(resolvedPath);
 
     for (const [moduleName, m] of Object.entries(modules)) {
