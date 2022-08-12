ef40f583c31e329bb1f81ebef43449ae93d2da55
Arjan van Wijk <thanarie@gmail.com>
2022-08-12 12:42:51 +0200

    Add code comments to pota-cli source (#67)
    * Add code comments to pota-cli source
    
    It helped me to understand the code better,
    and it should help others as well.

--- core/cli/src/main.ts
+++ core/cli/src/main.ts
@@ -8,17 +8,21 @@ const root = process.cwd();
 
 const { pota } = await readPackage(root);
 
+// read the "pota" field from the project's package.json
+// these scripts is what make commands available to pota to work with
 const commandModulePaths = Array.isArray(pota) ? [...pota] : [pota];
 
 if (commandModulePaths.length === 0) {
   throw new Error(`Could not find a configured pota scripts package in "${root}".`);
 }
 
-// load command modules
+// load commands that are available in the different pota scripts
 const commands = await loadCommands(root, commandModulePaths);
 
+// the main cli entry point
 const main = sade('pota');
 
+// Registers loaded commands (from all scripts) with sade, for availability within the CLI
 for (const [moduleName, commandModule] of Object.entries(commands)) {
   try {
     createCommand(main, root, commandModule);
