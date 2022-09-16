d6138ccc139c75d00f5c6f51732ac732f9f010cd
Jesse Linthorst <jesse@mediamonks.com>
2022-09-16 16:28:15 +0200

    react-base-template: add vite compatible index

--- templates/react-base/npm-scripts/postBuild.ts
+++ templates/react-base/npm-scripts/postBuild.ts
@@ -6,11 +6,16 @@ import { readFile, writeFile } from 'fs/promises';
  * injected scripts from the build.
  */
 (async () => {
-  console.info('[PostBuild] Cleaning up cra injected scripts');
+  console.info('[PostBuild] Altering index.html to (re)move cra/vite injected scripts');
 
   const index = path.resolve('./dist/index.html');
   const contents = await readFile(index, { encoding: 'utf-8' });
-  const cleaned = contents.replace(/<script[\w\s="]* src="[\w\d-\/]*static\/.*\/[\w\d\.-]*\.js"><\/script>/ig,'');
+  // Match webpack exports
+  let parsed = contents.replace(/<script[\w\s="]* src="[\w\d-\/]*static\/.*\/?[\w\d\.-]*\.js"><\/script>/ig,'');
+  // Match vite (rollup exports)
+  parsed = contents.replace(
+    /<script([\w\s="]*) src="([\w\d-\/]*assets\/.*\/?[\w\d\.-]*\.js)"><\/script>/ig,
+    '<meta name="app-script" content="$2" />');
 
-  await writeFile(index, cleaned, { encoding: 'utf-8' });
+  await writeFile(index, parsed, { encoding: 'utf-8' });
 })();
